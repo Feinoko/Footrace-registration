e18d5d3f819ce2d7430f44b27488113d4ff3bb2e
feinoko <feinoko@gmail.com>
2020-12-28 11:34:53 +0100

    defining classes - progress

---
+++ dist/app.js
@@ -0,0 +1,47 @@
+// CLASSES
+
+class Candidate {
+  constructor(fullName, gender, age) {
+    this.fullName = fullName;
+    this.gender = gender;
+    this.age = age;
+  }
+
+  getFullInfo() {
+    let pronoun;
+    let whatGender;
+    this.gender === 'female' || this.gender === 'Female' ? whatGender = 'female' : whatGender = 'male';
+    this.gender === 'female' || this.gender === 'Female' ? pronoun = 'she' : pronoun = 'he';
+    console.log(`Candidate's name is ${this.fullName}, ${pronoun} is a ${whatGender}, and is ${this.age} years old`);
+  }
+
+  getCategory() {
+
+  }
+
+}
+
+class UI {
+  // set all functions for UI interactions
+
+  addCandidate() {
+
+  }
+
+  removeCandidate() {
+
+  }
+
+  clearCandidates() {
+    
+  }
+
+}
+
+const jane = new Candidate('Jane Houston', 'Female', 25);
+
+console.log(jane);
+console.log(jane.getFullInfo());
+
+// EVENT HANDLERS
+
---
+++ memo.md
@@ -0,0 +1,36 @@
+Primary features & functionality
+===============================
+Add candidates to list
+Delete candidates from list
+Delete all candidates
+
+
+secondary/optional:
+------------------
+assign a race category depending on gender & age (male/female long/short and kids' race) for fairness
+
+problems/bugs to fix:
+------------------
+
+
+
+coding guidelines memo 
+=============
+KISS : Keep It Stupid Simple
+DRY : Dont Repeat Yourself
+YAGNI : You Aint Gonna Need it
+Break it down to very small bits
+Organise structure beforehand
+=============
+
+Current-task 
+==========
+
+---
+description/details : 
+========== 
+
+Stashed tasks (put on backburner to keep focused on current task)
+==========
+
+========== 
